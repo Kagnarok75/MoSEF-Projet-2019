d0ac2f83bb3758a47dc1107cef9bfb8af6f68f13
KarimBouabid <Karimbouabid@gmail.com>
2019-11-25 20:00:09 +0100

    ajout du 2eme argument -> filtre sur le texte recherché

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -5,12 +5,23 @@ read -p "Quel répertoire vous intéresse aujourd'hui ? " repertoire
 res=$(find / -name "$repertoire" -type d 2>/dev/null)
 if [ "$res" == "" ]
 then
-        echo "pas de repertoire existant contenant $repertoire "
+        echo  "pas de chemin existant contenant $repertoire "
 else
-        echo "$res"
+	echo   "Voici le(s) chemin(s) correspondant(s): $res"
 fi
 
-find ${res} -name $1
+if [ -n "$2" ]
+then
+	echo "Voici le(s) fichier(s) contenant le texte renseigné : "
+	find ${res} -name $1 -name $1 -exec grep -il $2 {} \;
+elif [ -n "$1" ] && [ -z "$2" ]
+then
+	echo "Voici le(s) fichier(s) recherché(s) : "
+	find ${res} -name $1
+fi
+
+
+
 
 
 
